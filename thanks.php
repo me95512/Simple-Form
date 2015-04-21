1d3aee3703e2c5ab9520397ff4471cefc5fdf948
me95512 <me95512@gmail.com>
2015-04-21 12:07:34 +0000

    Create thanks.php

---
+++ thanks.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+<title></title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+<link rel="stylesheet" type="text/css"href="print.css"/>
+</style>
+</head>
+<body>
+<?php # The thanks.php page developed by Mikalonet 19/04/2015
+$page_title = 'Thank You!';
+?>
+<h1 id="mainhead">Thank you!</h1>
+<p>Your data has now been saved in the database!</p>
+</div>
+</body>
+</html>
