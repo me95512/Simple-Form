2a3546451385c4f7f3390303f08f8da93ce252a1
me95512 <me95512@gmail.com>
2015-04-21 11:55:10 +0000

    Create index.js

---
+++ index.js
@@ -0,0 +1,33 @@
+// Mikalonet. 19/04/2015
+
+function Toggle(item) {
+   obj=document.getElementById(item);
+   visible=(obj.style.display!="none")
+   key=document.getElementById("x" + item);
+   if (visible) {
+     obj.style.display="none";
+     key.innerHTML="<img src='folder.jpeg' width='16' height='16' hspace='0' vspace='0' border='0'>";
+   } else {
+      obj.style.display="block";
+      key.innerHTML="<img src='folder.jpeg' width='16' height='16' hspace='0' vspace='0' border='0'>";
+   }
+}
+
+function Expand() {
+   divs=document.getElementsByTagName("DIV");
+   for (i=0;i<divs.length;i++) {
+     divs[i].style.display="block";
+     key=document.getElementById("x" + divs[i].id);
+     key.innerHTML="<img src='folder.jpg' width='16' height='16' hspace='0' vspace='0' border='0'>";
+   }
+}
+
+function Collapse() {
+   divs=document.getElementsByTagName("DIV");
+   for (i=0;i<divs.length;i++) {
+     divs[i].style.display="none";
+     key=document.getElementById("x" + divs[i].id);
+     key.innerHTML="<img src='folder.jpg' width='16' height='16' hspace='0' vspace='0' border='0'>";
+   }
+}
+
